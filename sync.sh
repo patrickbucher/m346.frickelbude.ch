005b710c8dd04fcaed845c02a94f2f5676b006de
Patrick Bucher <patrick.bucher@mailbox.org>
2024-10-19 15:32:54 +0200

    start using mathjax

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -av public/ --exclude 'js/mathjax' --exclude 'js/swagger-ui' debian@91.92.155.156:/home/m346.frickelbude.ch/public/
+rsync -av public/ --exclude 'js/swagger-ui' debian@91.92.155.156:/home/m346.frickelbude.ch/public/
